56410804a3a2f8df213250a4f990f03ebe483f63
sneha <sneha.paulson@cognizant.com>
2019-07-04 18:28:52 +0530

    Initial commit

---
+++ src/app/models/Project.model.ts
@@ -0,0 +1,14 @@
+
+export class Project{
+
+    
+    projectID : number;
+    project : string;
+    priority:number;
+    startDate : Date;
+    endDate : Date;
+    manager : string;
+    activeFlag:boolean;
+    noOfTasks:number;
+    completedTasks:number;
+    }
